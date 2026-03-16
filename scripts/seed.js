4581047c3f689f324c3266e7fc64b96c8d8d8b97
monsterrohafa <monsterrohafa@gmail.com>
2026-03-16 11:53:47 +0900

    auto: pre-work 2026-03-16 11:53

--- scripts/seed.js
+++ scripts/seed.js
@@ -30,6 +30,7 @@ const candidates = [
   { code: 'jhh', name: '전현희', party: '더불어민주당', password: '1234' },
   { code: 'ssw', name: '서승우', party: '국민의힘', password: '1234' },
   { code: 'kma', name: '강미애', party: '무소속', password: '1234' },
+  { code: 'jsh', name: '주순희', party: '더불어민주당', password: '1234' },
 ];
 
 async function seed() {
